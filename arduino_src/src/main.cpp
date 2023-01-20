77eae9b4e90e0e66f6dc99e6e175db83c4226fea
AJ Brandt <ajb3155@gmail.com>
2023-01-19 23:44:40 -0500

    First commit

---
+++ arduino_src/src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+
+const int redPin = 7;
+const int yellowPin = 6;
+const int greenPin = 5;
+const int bluePin = 4;
+
+void setup() {
+  pinMode(redPin, OUTPUT);
+  pinMode(yellowPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
